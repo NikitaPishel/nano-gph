38638f2a3e6524ce1c64370cc5b708c077a09c76
NikitaPishel <pogerproger@gmail.com>
2025-10-09 18:14:35 +0100

    fix: make colors protected from copy/move

--- include/ngph/colors.h
+++ include/ngph/colors.h
@@ -6,18 +6,21 @@
 
 namespace gph {
     class Colors {
+    private:
+        Colors();
+        
+        std::unordered_map<std::string, std::string> colorMap;
+        
     public:
         static const Colors& getInstance();
 
         std::string getColorId(std::string color) const;
 
-    private:
-        Colors();
-
-        std::unordered_map<std::string, std::string> colorMap;
-
         Colors(const Colors&) = delete;
         Colors& operator=(const Colors&) = delete;
+        Colors(Colors&&) = delete;
+        Colors& operator=(Colors&&) = delete;
+        
     };
 };
 
